05bbe4ff1c8448d2c5f5bf1fcfb8e577f3049645
bill <bembas06.mark@hotmail.com>
2019-03-17 18:05:11 +0200

    ADDED: FullCycleStats has basic statistic analysis

--- FullCycleStats.java
+++ FullCycleStats.java
@@ -1,12 +1,40 @@
-import java.util.Arrays;
 
 public class FullCycleStats {
 	private final Manager manager;
+	private double FullCycleSize;
+	private double RTP;
+	private double hitRate;
+	private int hits;
+	private double totalPrize;
+	private double volatility;
+	private double stDev;
+	StringBuilder sb = new StringBuilder();
+
+	public double getRTP() { return RTP; }
+
+	public double getHitRate() { return hitRate; }
+
+	public int getHits() { return hits; }
+
+	public double getTotalPrize() { return totalPrize; }
+
+	public double getVolatility() { return volatility; }
+
+	public double getStDev() { return stDev; }
+
+	public double getFullCycleSize() { return FullCycleSize; }
 
 	public FullCycleStats(Manager manager) {
 		if (manager == null)
 			throw new IllegalArgumentException("argument to constructor is null");
 		this.manager = manager;
+		RTP = 0;
+		hitRate = 0;
+		hits = 0;
+		totalPrize = 0;
+		volatility = 0;
+		stDev = 0;
+		FullCycleSize = 0;
 		final PaylineManager paylineManager = manager.getPaylineManager();
 		runFullCycle();
 	}
@@ -38,17 +66,19 @@ public class FullCycleStats {
 				int[] paylineSymbols = windowManager.getPaylineSymbols(payline);
 				int reward = paymentManager.getReward(paylineSymbols);
 
-				StringBuilder sb = new StringBuilder();
 				if (reward > 0) {
-					sb.append(windowStr);
-					windowStr = "";
-					sb.append(payline);
-					sb.append(" pays ");
-					sb.append(reward);
-					sb.append("\n");
-					sb.append("scatters in window: " + scattersum);
-					System.out.println(sb.toString());
+//                    sb.append(windowStr);
+//                    windowStr = "";
+//                    sb.append(payline);
+//                    sb.append(" pays ");
+//                    sb.append(reward);
+//                    sb.append("\n");
+//                    sb.append("scatters in window: " + scattersum);
+//                    System.out.println(sb.toString());
+					totalPrize += reward;
+					hits++;
 				}
+				FullCycleSize++;
 
 			}
 			// *****************************************************
@@ -75,13 +105,36 @@ public class FullCycleStats {
 			for (int i = next + 1; i < nReels; i++)
 				indices[i] = 0;
 		}
+
 	}
 
 	public static void main(String[] args) {
-		String filename = "IO/game.txt";
+		long start = System.currentTimeMillis();
+		String filename = "game.txt";
 		In in = new In(filename);
 		Manager manager = new Manager(in);
-		FullCycleStats fcs = new FullCycleStats(manager);
+		com.company.FullCycleStats fcs = new com.company.FullCycleStats(manager);
+
+		long elapsedTimeMillis = System.currentTimeMillis()-start;
+		float elapsedTimeMin = elapsedTimeMillis/(60*1000F);
+		fcs.sb.append(" Time :");
+		fcs.sb.append(elapsedTimeMin);
+		fcs.sb.append("\n");
+		fcs.sb.append(" Total prize :");
+		fcs.sb.append(fcs.getTotalPrize());
+		fcs.sb.append("\n");
+		fcs.sb.append(" Hits :");
+		fcs.sb.append(fcs.getHits());
+		fcs.sb.append("\n");
+		fcs.sb.append(" HitRate :");
+		fcs.hitRate = fcs.getFullCycleSize() / fcs.getHits();
+		fcs.sb.append(fcs.getHitRate());
+		fcs.sb.append("\n");
+		fcs.sb.append(" RTP :");
+		fcs.RTP = fcs.getTotalPrize()/fcs.getFullCycleSize();
+		fcs.sb.append( fcs.getRTP());
+		fcs.sb.append("\n");
+		System.out.println(fcs.sb.toString());
 	}
 
 }
